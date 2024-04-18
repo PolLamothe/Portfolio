17808b517546d3c9cf6482ccc8e98a06ccd97f12
Pol Lamothe <lamothepol@gmail.com>
2024-04-18 11:02:47 +0200

    IMG + router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import { createApp } from 'vue'
 import App from "./App.vue"
+import * as VueRouter from 'vue-router'
 import VueSplide from '@splidejs/vue-splide';
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes : [
+        {
+            path : '/accueil',
+            name : 'Accueil',
+            component : App
+        }
+    ]
+})
 
 
 const app = createApp(App)
 app.use(VueSplide)
-app.mount('#app')
+app.use(router).mount("#app")
+app.mount('app')
