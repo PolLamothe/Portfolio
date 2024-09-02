a1df27ba9b4757c9e3df705b6e6f8a23170d59d3
Pol Lamothe <lamothepol@gmail.com>
2024-09-02 20:12:30 +0200

    fixing base

--- Site/vite.config.js
+++ Site/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:"http://localhost:5173",
+  base:"./",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
